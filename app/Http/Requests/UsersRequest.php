0490c6d61db9266d792e23c9a8a64f6a50285c94
Mohamed Ahamed <mohamedx.28@gmail.com>
2021-05-04 20:29:54 +0100

    #Fix user form request

--- app/Http/Requests/UsersRequest.php
+++ app/Http/Requests/UsersRequest.php
@@ -30,16 +30,16 @@ class UsersRequest extends FormRequest
         return [
             'firstName' => 'required|string|max:191',
             'username' => 'required|unique:users|string|max:191',
-            'middleName' => 'string|max:191',
+            'middleName' => 'nullable|string|max:191',
             'lastName' => 'required|string|max:191',
-            'thumbnail' => 'image|mimes:jpg,jpeg,png',
+            'thumbnail' => 'nullable|image|mimes:jpg,jpeg,png',
             'phone' => 'required|unique:users,phone|digits:10',
-            'email' => 'string|regex:/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/|max:191',
-            'address' => 'string|max:191',
+            'email' => 'nullable|string|regex:/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/|max:191',
+            'address' => 'nullable|string|max:191',
             'state_id' => 'required|exists:states,id|integer',
             // 'birthDate' => ['date', new dateFormatRule()],
-            'birthDate' => 'date',
-            'gender' => ['required', Rule::in(['ذكر', 'انثى'])],
+            'birthDate' => 'nullable|date',
+            'gender' => ['required', 'string', Rule::in(['ذكر', 'انثى'])],
             'password' => 'required|string|max:191',
         ];
     }
@@ -52,5 +52,29 @@ class UsersRequest extends FormRequest
             $messages[] = $message;
         }
         throw new HttpResponseException(response()->json(['success' => false, 'errors' => $messages], 200));
+    }
+
+    public function messages()
+    {
+        return [
+            'firstName.required' => 'حقل الإسم الأول مطلوب',
+            'username.required' => 'حقل إسم المستخدم مطلوب',
+            'username.unique' => 'أسم المستخدم موجود بالفعل',
+            'middleName.string' => 'الإسم الأوسط يجب ان يكون نصي',
+            'lastName.required' => 'حقل الإسم الأخير مطلوب',
+            'thumbnail.image' => 'نوع الصورة يجب ان يكون ضمن jpg , jpeg, png',
+            'phone.required' => 'حقل الهاتف مطلوب',
+            'phone.unique' => 'رقم الهاتف مستخدم بالفعل',
+            'phone.digits' => 'يجب ان يكون طول رقم الهاتف 10 خانات',
+            'email.string' => 'الإيميل يجب ان يكون نصي',
+            'email.regex' => 'الإيميل غير مناسب',
+            'address.string' => 'العنوان يجب ان يكون نصي',
+            'state_id.required' => 'حقل الولاية مطلوب',
+            'state_id.exists' => 'حقل الولاية غير موجود في قاعدة البيانات',
+            'birthDate.date' => 'حقل تاريخ الميلاد يجب ان يكون تاريخ حقيقي',
+            'gender.required' => 'حقل النوع مطلوب',
+            'gender.string' => 'حقل النوع يجب ان يكون نصي',
+            'password.required' => 'حقل كلمة السر مطلوب',
+        ];
     }
 }
